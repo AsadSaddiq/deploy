d890563f3164fbc1e41f068413575d88952225cb
AsadSaddiq <asadryk2911@gmail.com>
2024-07-20 20:27:17 +0500

    first commit

--- myproject/build_files.sh
+++ myproject/build_files.sh
@@ -1,4 +1,11 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+
+echo "BUILD START"
+
+# Install dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
